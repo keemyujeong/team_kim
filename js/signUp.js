044ab2fc8b6f926109b46de19f8e8495d02bb085
keemyujeong <thinkrightkim@gmail.com>
2022-12-09 15:21:05 +0900

    login12/9

--- js/signUp.js
+++ js/signUp.js
@@ -5,20 +5,31 @@ function clickSubmit(){
 
   xhr.onreadystatechange = function(){
     if(xhr.readyState==4 && xhr.status==200){
-      document.getElementById('InputID').innerHTML = xhr.responseText;
-      document.getElementById('InputPassword2').innerHTML = xhr.responseText;
-      document.getElementById('InputName').innerHTML = xhr.responseText;
-      document.getElementById('InputEmail').innerHTML = xhr.responseText;
-      document.getElementById('InputZipcode').innerHTML = xhr.responseText;
-      document.getElementById('InputAddress').innerHTML = xhr.responseText;
-      document.getElementById('InputPhonecall1').innerHTML = xhr.responseText;
-      document.getElementById('InputPhonecall2').innerHTML = xhr.responseText;
-      document.getElementById('InputPhonecall3').innerHTML = xhr.responseText;
-
-      
+      alert("회원가입에 성공하셨습니다!")
     }
   }
 
-  xhr.open('POST', "")
+  var inputID = document.getElementById('InputID').value;
+  var inputPassword = document.getElementById('InputPassword').value;
+  var inputName = document.getElementById('InputName').value;
+  var inputEmail = document.getElementById('InputEmail').value;
+  var inputZipcode = document.getElementById('InputZipcode').value;
+  var inputAddress = document.getElementById('InputAddress').value;
+  var phonecall = document.getElementById('InputPhonecall1').value + document.getElementById('InputPhonecall2').value + document.getElementById('InputPhonecall3').value;
+
+
+  xhr.open('POST', "../signupPost.php", true);
+
+  xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  xhr.send(
+    "id=" + inputID + "&" +
+    "password=" + inputPassword + "&" +
+    "name=" + inputName + "&" +
+    "email=" + inputEmail + "&" +
+    "zipcode=" + inputZipcode + "&" +
+    "address=" + inputAddress + "&" +
+    "phonecall=" + phonecall
+    );
+
 }
 
--- php/signupPost.php
+++ php/signupPost.php
@@ -1,4 +1,31 @@
 <?php
+    header('content-Type:text/plain; charset=utf-8');
 
+    $id = $_POST['id'];
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $zipcode = $_POST['zipcode'];
+    $address = $_POST['address'];
+    $phonecall = $_POST['phonecall'];
+    
+    $name = addslashes($name);
+    $address = addslashes($address);
+
+    $now = date("Y-m-d H:i:s");
+
+    $db = mysqli_connect("localhost","kyjsoft","appleyummy1!","kyjsoft");
+    mysqli_query($db, "set names uft8");
+
+    $sql = "INSERT INTO plantBoard(id, password, name, zipcode, address, phonecall) VALUES ('$id', '$password', '$name', '$email', '$zipcode', '$address','$phonecall')";
+   
+    $result = mysqli_query($db, $sql);
+
+
+    if($result) echo "insert success";
+    else echo "insert failed";
+
+
+    mysqli_close($db);
 
 ?>
