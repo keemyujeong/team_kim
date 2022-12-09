63412db930e6ee61e81d5e1051c7027435e09a01
Ezra Lee <ezra.made.me@gmail.com>
2022-12-09 16:55:02 +0900

    221209

--- js/board.js
+++ js/board.js
@@ -1,1 +1,27 @@
-//
+//boardhead - 작성자 정보
+    //user_pic - 작성자 프로필 이미지
+    //user_name - 작성자명
+    //user_id - 작성자 아이디
+    //upload_date - 작성일
+
+//boardimage - 업로드된 이미지
+    //image - 이미지
+
+//actionboard - 유저 반응
+    //like - 좋아요 아이콘
+    //comment - 댓글 아이콘
+    //share - 공유하기 아이콘
+
+//boardarticle - 업로드된 글
+    //article_data - 글
+
+//comment - 댓글창
+    //form 요소 - addComment.php 액션
+    //comment_input - 댓글입력란
+    //add_com - 댓글 입력 완료 버튼
+
+    //div 요소 - 최신 댓글 보여주기
+    //user_pic - 댓글을 남긴 사용자의 프로필 이미지
+    //com_user - 댓글 단 사용자명
+    //com_content - 댓글 내용
+
