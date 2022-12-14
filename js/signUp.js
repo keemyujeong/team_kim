1461a05510de25b40afa51998718d7b717fa2166
keemyujeong <thinkrightkim@gmail.com>
2022-12-14 14:05:31 +0900

    12/14

--- js/signUp.js
+++ js/signUp.js
@@ -1,27 +1,25 @@
-
-
-// 비밀번호 유효성 검사
+// 비밀번호 유효성 검사 해야함
 
 
 function clickSubmit(){
 
   var xhr = new XMLHttpRequest();
-
   xhr.onreadystatechange = function(){
     if(xhr.readyState==4 && xhr.status==200){
       alert("회원가입에 성공하셨습니다!")
     }
   }
 
+
   var inputEmail = document.getElementById('InputEmail1').value + "@" + document.getElementById('InputEmail2').value;
   var inputPassword = document.getElementById('InputPassword').value;
   var inputName = document.getElementById('InputName').value;
   var inputZipcode = document.getElementById('InputZipcode1').value + document.getElementById('InputZipcode2').value;
   var inputAddress = document.getElementById('InputAddress').value;
-  var phonecall = document.getElementById('InputPhonecall1').value + document.getElementById('InputPhonecall2').value + document.getElementById('InputPhonecall3').value;
+  var phonecall = document.getElementById('InputPhonecall1').value + document.getElementById('InputPhonecall2').value + document.getElementById('inputPhonecall3').value;
 
 
-  xhr.open('POST', "../signupPost.php", true);
+  xhr.open('POST', "../php/signupPost.php", true);
 
   xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
   xhr.send(
@@ -32,7 +30,10 @@ function clickSubmit(){
     "address=" + inputAddress + "&" +
     "phonecall=" + phonecall
     );
+
 }
+
+
 
 function clickEmail(){
   alert("이메일로 인증번호가 전송되었습니다.")
